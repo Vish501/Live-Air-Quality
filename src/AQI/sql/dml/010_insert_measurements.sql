8e2069ff607e600e5e9e8f6a8057db6eb2edeebf
Vishal <76504332+Vish501@users.noreply.github.com>
2025-08-22 11:00:15 +0000

    Updated with comments for better understanding of the script

--- src/AQI/sql/dml/010_insert_measurements.sql
+++ src/AQI/sql/dml/010_insert_measurements.sql
@@ -1,3 +1,5 @@
+-- Script: 010_load_raw_air_quality.sql
+-- Purpose: Load CSV files (downloaded from public S3) into raw.air_quality_data with metadata.
 INSERT INTO raw.air_quality_data
 SELECT 
     location_id, 
@@ -11,6 +13,6 @@ SELECT
     "value",
     "month", 
     "year",
-    current_timestamp AS ingestion_datetime
-    '{{ file_name }}' AS file_name
-FROM read_csv('{{ data_file_path }}');
+    current_timestamp AS ingestion_datetime,   -- when loaded into DB
+    '{{ file_name }}' AS file_name             -- original file name from artifacts/
+FROM read_csv('{{ data_file_path }}');        -- e.g. artifacts/openaq_2025_08_21.csv
